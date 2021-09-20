31683665ff82e5a5803373d2dc8a15e815ef285b
Lane Harris <lrharris215@gmail.com>
2021-09-20 16:25:26 -0400

    Added test for print_board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,6 +9,18 @@ describe "Game" do
 
    it "prints a 3x3 board to the console" do 
       g = Game.new()
+      expect do 
       g.print_board()
+      end.to output("
+             |       | 
+         1   |   2   |   3
+       _____ | _____ | _____
+             |       |  
+         4   |   5   |   6
+       _____ | _____ | _____
+             |       | 
+         7   |   8   |   9
+             |       | 
+        \n").to_stdout
    end
 end
