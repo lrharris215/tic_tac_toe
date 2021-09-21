3077ef1bdd88634fd2c37d728b63edce3ecf0b6e
Lane Harris <lrharris215@gmail.com>
2021-09-21 14:32:52 -0400

    Ran standardrb

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,22 @@
 class Game
-    def initialize
-        puts "Welcome to Tic-Tac-Toe!"
-    end
+  def initialize
+    puts "Welcome to Tic-Tac-Toe!"
+  end
 
-    def print_board
-       puts "
-             |       | 
+  def print_board
+    puts "
+             |       |
          1   |   2   |   3
        _____ | _____ | _____
-             |       |  
+             |       |
          4   |   5   |   6
        _____ | _____ | _____
-             |       | 
+             |       |
          7   |   8   |   9
-             |       | 
+             |       |
         "
-    end
+  end
 end
 
-g = Game.new()
+g = Game.new
 g.print_board
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,26 +1,26 @@
 require "rspec"
-require_relative '../lib/game.rb'
+require_relative "../lib/game"
 
-describe "Game" do 
-   it "passes this example test" do 
-        pass = true
-        expect(pass).to eq(true)
-   end
+describe "Game" do
+  it "passes this example test" do
+    pass = true
+    expect(pass).to eq(true)
+  end
 
-   it "prints a 3x3 board to the console" do 
-      g = Game.new()
-      expect do 
-      g.print_board()
-      end.to output("
-             |       | 
+  it "prints a 3x3 board to the console" do
+    g = Game.new
+    expect do
+      g.print_board
+    end.to output("
+             |       |
          1   |   2   |   3
        _____ | _____ | _____
-             |       |  
+             |       |
          4   |   5   |   6
        _____ | _____ | _____
-             |       | 
+             |       |
          7   |   8   |   9
-             |       | 
+             |       |
         \n").to_stdout
-   end
+  end
 end
