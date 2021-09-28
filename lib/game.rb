995821d16ecbb4cd7934cb1da600b55916c84c35
Lane Harris <lrharris215@gmail.com>
2021-09-28 11:12:53 -0400

    Removed unnecessary game elements

--- lib/game.rb
+++ lib/game.rb
@@ -14,7 +14,7 @@ class Game
     @player_2 = players[1]
 
     @active_player = @player_1
-    @game_over = false
+    # @game_over = false
   end
 
   def start
@@ -23,22 +23,22 @@ class Game
 
   def play
     # use a while loop later to keep game going
-    while @game_over == false
+    # while @game_over == false
       print_board
-      @board.place_mark(@active_player.mark, @active_player.get_input(@display))
+      @board.place_mark(@player_1.mark, @player_1.get_input(@display))
       print_board
-      switch_player
-      @board.place_mark(@active_player.mark, @active_player.get_input(@display))
+      # switch_player
+      @board.place_mark(@player_2.mark, @player_2.get_input(@display))
       print_board
-      @game_over = true
-    end
+      # @game_over = true
+    # end
   end
 
   private
 
-  def switch_player
-    @active_player = @active_player === @player_1 ? @player_2 : @player_1
-  end
+  # def switch_player
+  #   @active_player = @active_player === @player_1 ? @player_2 : @player_1
+  # end
 
   def print_board
     @display.output(@presenter.print_board(@board))
