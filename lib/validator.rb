f88a651f20dc8e3bdcc60aac0f2febcad3e83b7d
Lane Harris <lrharris215@gmail.com>
2021-10-07 16:18:11 -0400

    Finished validator tests

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,21 +1,23 @@
 class Validator
+  def valid_move?(board, move)
+    valid_input?(move) && valid_position?(board, move)
+  end
 
-    def valid_move?(board, move)
-        valid_position?(board, move) && valid_input?(move)
+  private
+
+  def valid_position?(board, position)
+    # not taken by another piece
+    if /^[1-9]$/.match?(board.find_position(position).to_s)
+      return true
     end
+    false
+  end
 
-    def valid_position?(board, position)
-        # not taken by another piece
-        
+  def valid_input?(input)
+    # num from 1 - 9
+    if /^[1-9]$/.match?(input)
+      return true
     end
-
-    def valid_input?(input)
-        #num from 1 - 9
-        if input.match(/^[1-9]$/)
-            return true
-        end
-        false
-    end
-
-
+    false
+  end
 end
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -9,6 +9,10 @@ class TestBoard
   def place_mark(mark, position)
     @cells[position - 1] = mark
   end
+
+  def find_position(num)
+    @cells[num.to_i - 1]
+  end
 end
 
 class TestPlayer
@@ -23,24 +27,27 @@ class TestPlayer
 end
 
 describe "Validator" do
-    validator = Validator.new()
-    context "valid_input?" do 
-        it "Accepts valid player input" do
-            expect(validator.valid_input?("4")).to be(true)
-        end
-        it "Rejects invalid player input" do 
-            expect(validator.valid_input?("100")).to be(false)
-            expect(validator.valid_input?("ABC")).to be(false)
-        end
+  subject(:validator) { Validator.new }
+  let(:board) { TestBoard.new }
+  context "valid_move?" do
+    it "Accepts valid player input" do
+      expect(validator.valid_move?(board, "3")).to be(true)
+    end
+    it "Rejects invalid player input" do
+      expect(validator.valid_move?(board, "100")).to be(false)
+      expect(validator.valid_move?(board, "ABC")).to be(false)
     end
 
-    context "valid_position" do 
-        it "Rejects input if space is already taken" do
-            board = TestBoard.new()
-            player1 = TestPlayer.new("Z")
-
-            board.place_mark(player1.mark, 1)
-            expect(validator.valid_position?(board, 1)).to be(false)
-        end
+    it "Accepts input if the space is has not been taken" do
+      board = TestBoard.new
+      expect(validator.valid_move?(board, "1")).to be(true)
     end
+    it "Rejects input if space is already taken" do
+      board = TestBoard.new
+      player1 = TestPlayer.new("Z")
+
+      board.place_mark(player1.mark, 1)
+      expect(validator.valid_move?(board, "1")).to be(false)
+    end
+  end
 end
