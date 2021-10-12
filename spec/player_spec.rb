b2b64e78d945e3415ea99a446c52ef97f877299a
Lane Harris <lrharris215@gmail.com>
2021-10-12 15:27:17 -0400

    linter

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,6 @@
 describe "Player" do
-  subject(:player) { Player.new(TestValidator.new, "Z")}
+  subject(:player) { Player.new(TestValidator.new, "Z") }
   it "Receives input from the player" do
-    
     allow(player).to receive(:get_input).and_return("100")
     input = player.get_input
 
