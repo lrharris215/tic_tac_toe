9fc050ce4062afb015d87cf76173f72367711949
Lane Harris <lrharris215@gmail.com>
2021-10-12 13:28:40 -0400

    Displayer no longer receives input, only Receiver does

--- lib/player.rb
+++ lib/player.rb
@@ -2,26 +2,28 @@ require_relative "./constants"
 
 class Player
   attr_reader :mark
-  def initialize(validator, mark)
+  def initialize(receiver, mark)
     # X or O
     @mark = mark
-    @validator = validator
+    # @validator = validator
+    @receiver = receiver
   end
 
   def get_input(display)
     display.output("It is Player #{@mark}'s turn!")
     display.output(GET_PLAYER_INPUT)
-    display.input
+    # display.input
+    receiver.get_player_move(mark)
   end
 
-  def get_valid_input(display, board, converter)
-    input = get_input(display)
-    # input is a string
-    until @validator.valid_move?(board, input, converter)
-      display.output(INPUT_ERROR)
-      input = get_input(display)
-    end
-    # change it to an int
-    converter.numberfy(input)
-  end
+#   def get_valid_input(display, board, converter)
+#     input = get_input(display)
+#     # input is a string
+#     until @validator.valid_move?(board, input, converter)
+#       display.output(INPUT_ERROR)
+#       input = get_input(display)
+#     end
+#     # change it to an int
+#     converter.numberfy(input)
+#   end
 end
--- lib/receiver.rb
+++ lib/receiver.rb
@@ -1,11 +1,21 @@
 class Receiver
-    def initialize(display, validator)
+    def initialize(input_function, validator, converter)
         @display = display
-        
+        @input_function = input_function
         @validator = validator
+
+        @converter = converter
+    end
+    def input
+        @input_function.gets.chomp
+    end
+    def get_player_move(board, mark)
+        move = input
+
+        until @validator.valid_move?(board, move, @converter)
+            move = input
+        end
+        converter.numberfy(move)
     end
 
-    def get_player_move()
-        move = @display.input
-    end
 end
