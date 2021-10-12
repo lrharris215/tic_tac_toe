d5284947cd70bd90a1c402537526f65dabcf6881
Lane Harris <lrharris215@gmail.com>
2021-10-12 15:25:53 -0400

    Added test for ComputerReceiver, refactored Player test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,9 @@
 describe "Player" do
+  subject(:player) { Player.new(TestValidator.new, "Z")}
   it "Receives input from the player" do
-    @player = Player.new(TestValidator.new, "Z")
-    allow(@player).to receive(:get_input).and_return("100")
-    input = @player.get_input
+    
+    allow(player).to receive(:get_input).and_return("100")
+    input = player.get_input
 
     expect(input).to eq("100")
   end
