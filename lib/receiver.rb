acc8aec991eb465a360d592df703c9467761d846
Lane Harris <lrharris215@gmail.com>
2021-10-13 14:17:42 -0400

    removed more comments

--- lib/receiver.rb
+++ lib/receiver.rb
@@ -1,8 +1,6 @@
 class Receiver
   def initialize(input_function, converter)
     @input_function = input_function
-    # @validator = validator
-
     @converter = converter
   end
 
@@ -10,29 +8,7 @@ class Receiver
     @input_function.gets.chomp
   end
 
-  # def get_player_move(display, board, mark)
-  #   move = input
-
-  #   until @validator.valid_move?(board, move, @converter)
-  #     display.output(INPUT_ERROR)
-  #     move = input
-  #   end
-  #   @converter.numberfy(move)
-  # end
-
-  # def get_player_two_choice(display)
-  #   choice = input
-  #   until @validator.valid_player2?(choice)
-  #     display.output(INPUT_ERROR)
-  #     choice = input
-  #   end
-  #   choice
-  # end
-
   def get_player_input(validator, display)
-    # move val or player val
-    # move val would have the board inside it?
-    # would thsi mean i'd need to create a new val every time?
     p_input = input
     until validator.valid?(p_input)
       display.output(INPUT_ERROR)
