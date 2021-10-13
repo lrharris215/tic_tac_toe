01237d64a3ea277eb86f5aaae509105695a6794b
Lane Harris <lrharris215@gmail.com>
2021-10-13 13:18:00 -0400

    cWIP: created diff validators

---
+++ lib/move_validator.rb
@@ -0,0 +1,30 @@
+class MoveValidator
+    def initialize(board, converter)
+        @converter = converter
+        @board = board
+    end
+    def valid?(input)
+  
+        valid_input?(input, @converter) && valid_position?(@board, input, @converter)
+    end
+
+
+    
+    private
+
+  def valid_position?(board, position, converter)
+    # not taken by another piece
+    if /^[1-9]$/.match?(converter.stringify(board.find_position(converter.numberfy(position))))
+      return true
+    end
+    false
+  end
+
+  def valid_input?(input, converter)
+    # num from 1 - 9
+    if /^[1-9]$/.match?(converter.stringify(input))
+      return true
+    end
+    false
+  end
+end
---
+++ lib/player_validator.rb
@@ -0,0 +1,5 @@
+class PlayerValidator
+    def valid?(choice)   
+    /\bhuman\b/i.match?(choice) || /\bcomputer\b/i.match?(choice)
+    end
+end
