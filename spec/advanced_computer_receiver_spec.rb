c2f2760b42724edf14b3aedcb25e8c91649431a7
Lane Harris <lrharris215@gmail.com>
2021-10-18 17:35:22 -0400

    Added real validator to test file

--- spec/advanced_computer_receiver_spec.rb
+++ spec/advanced_computer_receiver_spec.rb
@@ -6,7 +6,7 @@ describe "AdvancedComputerReceiver" do
   subject(:advanced) { AdvancedComputerReceiver.new(board, game_checker) }
   let(:computer) { TestPlayer.new(advanced, "O") }
   let(:human) { TestPlayer.new(TestReceiver, "X") }
-  let(:v) { TestValidator.new }
+  let(:validator) { MoveValidator.new(board, TestConverter.new) }
   let(:display) { TestDisplay.new }
 
   def fill_corners(board, mark)
@@ -20,13 +20,13 @@ describe "AdvancedComputerReceiver" do
       board.place_mark(computer.mark, 1)
       board.place_mark(computer.mark, 2)
 
-      expect(advanced.get_player_input(v, display, computer, human)).to eq(3)
+      expect(advanced.get_player_input(validator, display, computer, human)).to eq(3)
     end
 
     it "will block a human player from winning" do
       board.place_mark(human.mark, 1)
       board.place_mark(human.mark, 4)
-      expect(advanced.get_player_input(v, display, computer, human)).to eq(7)
+      expect(advanced.get_player_input(validator, display, computer, human)).to eq(7)
     end
 
     it "will create a fork" do
@@ -34,7 +34,7 @@ describe "AdvancedComputerReceiver" do
       board.place_mark(human.mark, 5)
       board.place_mark(computer.mark, 4)
 
-      expect(advanced.get_player_input(v, display, computer, human)).to eq(1)
+      expect(advanced.get_player_input(validator, display, computer, human)).to eq(1)
     end
 
     it "will block the creation of a fork" do
@@ -42,28 +42,38 @@ describe "AdvancedComputerReceiver" do
       board.place_mark(computer.mark, 5)
       board.place_mark(human.mark, 4)
 
-      expect(advanced.get_player_input(v, display, computer, human)).to eq(1)
+      expect(advanced.get_player_input(validator, display, computer, human)).to eq(1)
     end
 
     it "will pick the center square if it's available" do
-      expect(advanced.get_player_input(v, display, computer, human)).to eq(5)
+      expect(advanced.get_player_input(validator, display, computer, human)).to eq(5)
     end
 
     it "will pick the opposite corner from the human player" do
       board.place_mark(human.mark, 1)
       allow(advanced).to receive(:center).and_return(false)
-      expect(advanced.get_player_input(v, display, computer, human)).to eq(9)
+      expect(advanced.get_player_input(validator, display, computer, human)).to eq(9)
     end
 
     it "will pick a random corner" do
       allow(advanced).to receive(:center).and_return(false)
-      expect([1, 3, 7, 9]).to include(advanced.get_player_input(v, display, computer, human))
+      expect([1, 3, 7, 9]).to include(advanced.get_player_input(validator, display, computer, human))
     end
 
     it "will pick a random side" do
       fill_corners(board, human.mark)
       allow(advanced).to receive(:center).and_return(false)
-      expect([2, 4, 6, 8]).to include(advanced.get_player_input(v, display, computer, human))
+      expect([2, 4, 6, 8]).to include(advanced.get_player_input(validator, display, computer, human))
+    end
+
+    it "will pass this test" do 
+        board.place_mark(human.mark, 5)
+        board.place_mark(computer.mark, 9)
+        board.place_mark(human.mark, 3)
+        board.place_mark(computer.mark, 7)
+        board.place_mark(human.mark, 8)
+        expect(advanced.get_player_input(validator, display, computer, human)).to eq(2)
+        
     end
   end
 end
